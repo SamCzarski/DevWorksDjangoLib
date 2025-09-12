bdbf6f6d495e2aed1eb783b39a828a094b5b13dd
SamCzarski <sczarski@informationmediary.com>
2025-09-12 15:30:46 -0400

    added some docs & added a general viewset and pagination

--- DevWorksDjangoLib/_version.py
+++ DevWorksDjangoLib/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.1'
-__version_tuple__ = version_tuple = (0, 0, 1)
+__version__ = version = '0.0.2.dev0+g9c7b21a58.d20250912'
+__version_tuple__ = version_tuple = (0, 0, 2, 'dev0', 'g9c7b21a58.d20250912')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'g9c7b21a58'
