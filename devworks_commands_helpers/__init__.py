2a74f198239cf44bd8d405955df7cc133b1ddc88
SamCzarski <sczarski@informationmediary.com>
2025-09-13 17:26:42 -0400

    init fixed

--- devworks_commands_helpers/__init__.py
+++ devworks_commands_helpers/__init__.py
@@ -1,3 +1,3 @@
 def format_logging():
-    from certiscan_commands_helpers.logging_struct import run
+    from devworks_commands_helpers.logging_struct import run
     run()
--- devworks_django/__init__.py
+++
@@ -1,3 +0,0 @@
-def format_logging():
-    from certiscan_commands_helpers.logging_struct import run
-    run()
